30cf18c62c6fee9f4b356492688041269133fc15
DishaunJose <dishaun.jose07@gmail.com>
2026-03-03 10:22:28 +0530

    UC4: Introduced CharacterPattern class for banner letters

--- UC2PrintBanner.java
+++ UC2PrintBanner.java
@@ -1,17 +1,48 @@
 public class UC2PrintBanner {
 
     public static void main(String[] args) {
-        printBanner();
-    }
 
-    public static void printBanner() {
+        String[] O = {
+            "  *****  ",
+            " **   ** ",
+            "**     **",
+            "**     **",
+            "**     **",
+            " **   ** ",
+            "  *****  "
+        };
 
-        System.out.println("  *****  " + "  *****  " + " ******  " + "  *****  ");
-        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **   ** ");
-        System.out.println("**     **" + "**     **" + "**     **" + "**        ");
-        System.out.println("**     **" + "**     **" + "******   " + "  *****  ");
-        System.out.println("**     **" + "**     **" + "**        " + "       **");
-        System.out.println(" **   ** " + " **   ** " + "**        " + " **   ** ");
-        System.out.println("  *****  " + "  *****  " + "**        " + "  *****  ");
+        String[] P = {
+            " ******  ",
+            " **   ** ",
+            "**     **",
+            "******   ",
+            "**        ",
+            "**        ",
+            "**        "
+        };
+
+        String[] S = {
+            "  *****  ",
+            " **   ** ",
+            "**        ",
+            "  *****  ",
+            "       **",
+            " **   ** ",
+            "  *****  "
+        };
+
+        CharacterPattern letterO = new CharacterPattern(O);
+        CharacterPattern letterP = new CharacterPattern(P);
+        CharacterPattern letterS = new CharacterPattern(S);
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(
+                letterO.getLine(i) +
+                letterO.getLine(i) +
+                letterP.getLine(i) +
+                letterS.getLine(i)
+            );
+        }
     }
 }
