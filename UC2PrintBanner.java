6a263c382bb7ed63114d232121364ef2f0ee1d7f
DishaunJose <dishaun.jose07@gmail.com>
2026-03-03 10:16:58 +0530

    UC3: Refactored banner logic into separate method

--- UC2PrintBanner.java
+++ UC2PrintBanner.java
@@ -1,6 +1,10 @@
 public class UC2PrintBanner {
 
     public static void main(String[] args) {
+        printBanner();
+    }
+
+    public static void printBanner() {
 
         System.out.println("  *****  " + "  *****  " + " ******  " + "  *****  ");
         System.out.println(" **   ** " + " **   ** " + " **   ** " + " **   ** ");
